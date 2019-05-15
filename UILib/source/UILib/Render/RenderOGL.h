04e662d6c536eb869d272bc2182eba460f33df03
Leo Benaducci <leobenaducci@hotmail.com>
2019-05-15 07:10:59 -0400

    Added RenderContext

--- UILib/source/UILib/Render/RenderOGL.h
+++ UILib/source/UILib/Render/RenderOGL.h
@@ -1,5 +1,6 @@
 
 #include "Render.h"
+#include "RenderContext.h"
 
 class RenderOGL : public IRender
 {
@@ -8,3 +9,19 @@ public:
 	virtual void OnWindowDestroyed(IWindow*) override;
 	virtual void PaintWindow(IWindow*) override;
 };
+
+class RenderContextOGL : public IRenderContext
+{
+public:
+	void DrawLine(const struct Vector2& Start, const struct Vector2& End, const struct Vector4& Color) {}
+	void DrawRect(const struct Vector2& Pos, const struct Vector2& Size, const Image& Image, const struct Vector4& Color) {}
+	void DrawString(const struct Vector2& Pos, const struct Font& Font, wchar_t* Text) {}
+
+	void SetClipRect(const struct Vector2& Min, const struct Vector2& Max) {}
+};
+
+IRenderContext& GetRenderContext()
+{
+	static RenderContextOGL RenderContext;
+	return RenderContext;
+}
